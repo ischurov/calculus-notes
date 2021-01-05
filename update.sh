42ea0b91c9de5f39bf1838363e9e43c301d1d6cc
Ilya V. Schurov <ilya@schurov.com>
2021-01-05 21:58:56 +0300

    mobile-friendly layout

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-qqmathbook build --node-mathjax --base-url http://math-info.hse.ru/calculus-1
+qqmathbook build --node-mathjax --base-url http://calculus.mathbook.info/
 cd build
 rsync -avz * mi:/srv/www/math-info/calculus-1/
